ebf412b212eb11d738959ef81693b5bee94904ad
benoit <benoit.falquet@roche.com>
2022-02-14 17:59:57 +0100

    correct import

--- R/package.R
+++ R/package.R
@@ -3,10 +3,8 @@
 #'
 #' @importFrom assertthat assert_that on_failure<-
 #' @importFrom tibble tribble
-#' @importFrom checkmate assert_multi_class
-#' @importFrom checkmate assert_character assert_factor
+#' @import checkmate 
 #' @import dplyr
-#' @import dm
 #' @importFrom forcats fct_explicit_na
 #' @importFrom methods is
 #' @importFrom stats setNames
