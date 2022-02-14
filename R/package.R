a8f5df15a1a314ebed6d33aace049f4577e927b3
benoit <benoit.falquet@roche.com>
2022-02-14 18:09:46 +0100

    buildignore

--- R/package.R
+++ R/package.R
@@ -4,7 +4,7 @@
 #' @importFrom assertthat assert_that on_failure<-
 #' @importFrom tibble tribble
 #' @import checkmate 
-#' @import dplyr
+#' @import testthat
 #' @importFrom forcats fct_explicit_na
 #' @importFrom methods is
 #' @importFrom stats setNames
