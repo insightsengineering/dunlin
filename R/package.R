42ceacbecf8fb52446f89683a120f747297b0f89
benoit <benoit.falquet@roche.com>
2022-02-14 11:22:41 +0100

    document

---
+++ R/package.R
@@ -0,0 +1,10 @@
+
+#' Chevron Package
+#'
+#' @importFrom assertthat assert_that on_failure<-
+#' @importFrom tibble tribble
+#' @import dplyr
+#' @import dm
+#' @importFrom methods is
+#' @importFrom stats setNames
+"_PACKAGE"
