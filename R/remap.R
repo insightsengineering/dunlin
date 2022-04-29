f0afebf5a89a36e6d19abcdfb538da69bb023e7d
b_falquet <64274616+BFalquet@users.noreply.github.com>
2022-04-30 00:32:28 +0200

    Bug resolve all case (#19)
    * bug fix
    
    * styler
    
    * fix bug leading to level duplication
    
    * import :=
    
    * remove trailing white space
    
    Co-authored-by: benoit <benoit.falquet@roche.com>

--- R/remap.R
+++ R/remap.R
@@ -38,7 +38,7 @@
 #'       "22" = "2"
 #'     )
 #'   ),
-#'   All = list(
+#'   ALL = list(
 #'     fact = list(
 #'       "F1" = "f1",
 #'       "F2" = "f2",
@@ -52,13 +52,20 @@
 #' )
 #'
 #' res <- remap(db, my_map)
-remap <- function(db, map) {
+remap <- function(db, map = NULL) {
+  if (is.null(map)) {
+    return(db)
+  }
+
   assert_remap(map)
 
   remap_tab <- intersect(names(map), names(db))
-  if ("ALL" %in% toupper(names(map))) remap_tab <- c("All", remap_tab)
+  if ("ALL" %in% toupper(names(map))) {
+    remap_tab <- c("All", remap_tab)
+    names(map)[toupper(names(map)) == "ALL"] <- "All"
+  }
 
-  # iterate over highest map level (tab)
+  # iterate over highest map level (tab).
   for (tab in remap_tab) {
     local_map <- map[[tab]]
 
@@ -93,6 +100,7 @@ remap <- function(db, map) {
 #'   not a valid column of the selected `tab` in the object, the original object is returned. This behavior is desirable
 #'   when a variable that exists in most but not all tables has to be re coded.
 #'
+#' @importFrom rlang :=
 #'
 #' @return a `dm` object with re coded variables as factor.
 #' @export
@@ -131,6 +139,7 @@ h_remap_tab <- function(db, tab, col, dic_map) {
   ori_lvl <- levels(as.factor(ori))
   unknow_lvl <- setdiff(ori_lvl, names(dic_map))
   new_level <- c(unique(dic_map), unknow_lvl)
+  new_level <- unique(new_level)
 
   is_na <- which(is.na(new))
   new[is_na] <- ori_char[is_na]
@@ -195,15 +204,17 @@ assert_remap <- function(map) {
   msg <- NULL
 
   # assert unique table names
-  assert_list(map)
+  assert_list(map, min.len = 1)
   res <- duplicated(names(map))
   if (any(res)) {
     msg <- paste("\nDuplicated table names:", toString(unique(names(map)[res])))
   }
 
-  # assert unique variable name in each table
+
   var_remap <- lapply(map, names)
-  res <- unlist(lapply(var_remap, function(x) test_character(x, unique = TRUE)))
+
+  # assert unique variable name in each table
+  res <- unlist(lapply(var_remap, function(x) test_character(x, unique = TRUE, null.ok = TRUE)))
   if (!all(res)) {
     msg <- c(msg, paste("\nDuplicated Variable name inside table:", toString(unique(names(var_remap)[!res]))))
   }
--- tests/testthat/test-remap.R
+++ tests/testthat/test-remap.R
@@ -91,7 +91,7 @@ test_that("h_remap_tab works as expected", {
 
 # remap ----
 
-test_that("remap works as expected", {
+test_that("remap works as expected with other All spelling", {
   df1 <- data.frame(
     "char" = c("a", "b", NA, "a", "k", "x"),
     "fact" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
@@ -119,6 +119,64 @@ test_that("remap works as expected", {
       )
     ),
     All = list(
+      fact = list(
+        "F1" = "f1",
+        "F2" = "f2",
+        "FX" = "fx",
+        "<Missing>" = NA
+      ),
+      other = list(
+        "x" = "X"
+      )
+    )
+  )
+
+  res <- expect_silent(remap(db, my_map))
+
+  expected_char <- factor(c("A", "B", NA, "A", "A", "x"), levels = c("A", "B", "x"))
+  expect_identical(res$df1$char, expected_char)
+
+  expected_num <- factor(c(11, 22, 3, 4, 5, 6), levels = c(11, 22, 3, 4, 5, 6))
+  expect_identical(res$df2$num, expected_num)
+
+  expected_fact <- factor(
+    c("F1", "F2", "<Missing>", "<Missing>", "F1", "F1"),
+    levels = c("F1", "F2", "FX", "<Missing>")
+  )
+  expect_identical(res$df1$fact, expected_fact)
+  expect_identical(res$df2$fact, expected_fact)
+})
+
+
+
+test_that("remap works as expected", {
+  df1 <- data.frame(
+    "char" = c("a", "b", NA, "a", "k", "x"),
+    "fact" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
+    "logi" = c(NA, FALSE, TRUE, NA, FALSE, NA)
+  )
+  df2 <- data.frame(
+    "char" = c("a", "b", NA, "a", "k", "x"),
+    "fact" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
+    "num" = 1:6
+  )
+
+  db <- dm::dm(df1, df2)
+
+  my_map <- list(
+    df1 = list(
+      char = list(
+        "A" = c("a", "k"),
+        "B" = "b"
+      )
+    ),
+    df2 = list(
+      num = list(
+        "11" = "1",
+        "22" = "2"
+      )
+    ),
+    ALL = list(
       fact = list(
         "F1" = "f1",
         "F2" = "f2",
