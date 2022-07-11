cd29fd0613df244a14e9ab2e9ff30e9d830a2e31
benoit <benoit.falquet@roche.com>
2022-07-11 15:39:23 +0200

    initial code

---
+++ tests/testthat/_snaps/propagate.md
@@ -0,0 +1,11 @@
+# propagate works as expected
+
+    Code
+      res
+    Output
+      -- Metadata --------------------------------------------------------------------
+      Tables: `airlines`, `airports`, `flights`, `planes`, `weather`
+      Columns: 54
+      Primary keys: 4
+      Foreign keys: 4
+
---
+++ tests/testthat/test-propagate.R
@@ -0,0 +1,37 @@
+
+test_that("propagate works as expected", {
+  df1 <- data.frame(
+    "id" = c("a", "b", NA, "a", "k", "x"),
+    "id2" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
+    "val" = letters[1:6]
+  )
+  df2 <- data.frame(
+    "id" = c("a", "b", NA, "a", "k", "x"),
+    "id2" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
+    "num" = 1:6
+  )
+
+  db <- dm::dm(df1, df2)
+
+  res <- propagate(db, "df1", "val", c("id", "id2"))
+  expect_snaphot(res)
+})
+
+test_that("propagate works as expected in safe mode", {
+  df1 <- data.frame(
+    "id" = c("a", "a", NA, "a", "k", "x"),
+    "id2" = factor(c("f1", "f1", NA, NA, "f1", "f1")),
+    "val" = letters[1:6]
+  )
+  df2 <- data.frame(
+    "id" = c("a", "b", NA, "a", "k", "x"),
+    "id2" = factor(c("f1", "f2", NA, NA, "f1", "f1")),
+    "num" = 1:6
+  )
+
+  db <- dm::dm(df1, df2)
+
+  expect_error(propagate(db, "df1", "val", c("id", "id2"), safe = TRUE), "Duplicated key")
+  res <- propagate(db, "df1", "val", c("id", "id2"), safe = FALSE)
+  expect_snapshot(res)
+})
