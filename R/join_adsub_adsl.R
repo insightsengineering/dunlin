5b264f5808b0a983e941d0f022a392276dd99bdb
b_falquet <64274616+BFalquet@users.noreply.github.com>
2022-11-25 09:22:08 +0100

    40 [Feature Request] create join_adsl_adsub (#41)
    https://github.com/insightsengineering/dunlin/issues/40
    close #40 
    
    - new function
    - tests
    - documentation / pkgdown
    
    thank you for the review
    
    Co-authored-by: benoit <benoit.falquet@roche.com>

---
+++ R/join_adsub_adsl.R
@@ -0,0 +1,135 @@
+#' Join `adsub` to `adsl`
+#'
+#' @param adam_db (`dm`) object input with an `adsl` and `adsub` table.
+#' @param keys (`character`) the name of the columns in `adsl` uniquely identifying a row.
+#' @param continuous_var (`character`) the value of a parameter in the `PARAMCD` column of the `adsub` table from which
+#'   columns containing continuous values should be created. If `"all"`, all parameter values are selected, if `NULL`,
+#'   none are selected.
+#' @param categorical_var (`character`) the value of a parameter in the `PARAMCD` column of the `adsub` table from which
+#'   columns containing categorical values should be created. If `"all"`, all parameter values are selected, if `NULL`,
+#'   none are selected.
+#' @param continuous_suffix (`string`) the suffixes to add to the newly generated columns containing continuous values.
+#' @param categorical_suffix (`string`) the suffixes to add to the newly generated columns containing categorical
+#'   values.
+#'
+#' @return a `dm` object with new columns in the `adsl` table.
+#'
+#' @export
+#'
+#' @examples
+#' adsl <- data.frame(
+#'   USUBJID = c("S1", "S2", "S3", "S4"),
+#'   STUDYID = "My_study",
+#'   AGE = c(60, 44, 23, 31)
+#' )
+#'
+#' adsub <- data.frame(
+#'   USUBJID = c("S1", "S2", "S3", "S4", "S1", "S2", "S3"),
+#'   STUDYID = "My_study",
+#'   PARAM = c("weight", "weight", "weight", "weight", "height", "height", "height"),
+#'   PARAMCD = c("w", "w", "w", "w", "h", "h", "h"),
+#'   AVAL = c(98, 75, 70, 71, 182, 155, 152),
+#'   AVALC = c(">80", "<=80", "<=80", "<=80", ">180", "<=180", "<=180")
+#' )
+#'
+#' db <- dm::dm(adsl, adsub)
+#'
+#' db <- dm::dm_add_pk(db, adsl, STUDYID)
+#' db <- dm::dm_add_fk(db, adsub, STUDYID, adsl)
+#'
+#' x <- join_adsub_adsl(adam_db = db)
+#' x <- join_adsub_adsl(adam_db = db, continuous_var = c("w", "h"), categorical_var = "h")
+join_adsub_adsl <- function(adam_db,
+                            keys = c("USUBJID", "STUDYID"),
+                            continuous_var = "all",
+                            categorical_var = "all",
+                            continuous_suffix = "",
+                            categorical_suffix = "_CAT") {
+  checkmate::assert_class(adam_db, "dm")
+  checkmate::assert_names(names(adam_db), must.include = c("adsl", "adsub"))
+  checkmate::assert_names(names(adam_db$adsub), must.include = c("PARAM", "PARAMCD", "AVAL", "AVALC", keys))
+  checkmate::assert_names(names(adam_db$adsl), must.include = keys)
+  checkmate::assert_string(continuous_suffix)
+  checkmate::assert_string(categorical_suffix)
+
+  value_col <- c("AVAL", "AVALC")
+  vars_ls <- list(continuous_var, categorical_var)
+  suffix_ls <- list(continuous_suffix, categorical_suffix)
+
+  vars_ls <- lapply(vars_ls, function(x) {
+    if (identical(x, "all")) {
+      unique(adam_db$adsub$PARAMCD)
+    } else {
+      x
+    }
+  })
+
+  # Create new names.
+  vars_nam <- mapply(
+    function(x, y) {
+      if (!is.null(x)) {
+        names(x) <- paste0(x, y)
+        x
+      } else {
+        NULL
+      }
+    },
+    vars_ls,
+    suffix_ls,
+    SIMPLIFY = FALSE
+  )
+
+
+  # Test if new columns already exist in adsl.
+  final_names <- unique(sapply(vars_nam, names))
+  already_in_adsl <- final_names %in% colnames(adam_db$adsl)
+  if (any(already_in_adsl)) {
+    warning(
+      paste(
+        toString(final_names[already_in_adsl]),
+        "already exist in adsl, the name will default to another values.
+Please change `continuous_suffix` or `categorical_suffix` to avoid automatic reneaming"
+      )
+    )
+  }
+
+  # Test if categorical and continuous column will result in the same column name.
+  final_names_ls <- lapply(vars_nam, names)
+  in_both <- final_names_ls[[1]] %in% final_names_ls[[2]]
+  if (any(in_both)) {
+    warning(
+      paste(
+        toString(final_names_ls[[1]][in_both]),
+        "are new columns for continuous and categorical variable,
+Please set different `continuous_suffix` or `categorical_suffix`
+or select different columns to avoid automatic renaming."
+      )
+    )
+  }
+
+  adsub_wide_ls <-
+    adam_db$adsub %>%
+    poly_pivot_wider(id = keys, param_from = "PARAMCD", value_from = value_col, labels_from = "PARAM")
+
+  # Generate a non-existing table name.
+  new_tab_nam <- tail(make.names(c(names(adam_db), "adsub"), unique = TRUE), 1)
+
+  for (i in seq_along(value_col)) {
+    adsub_df <- adsub_wide_ls[[value_col[i]]]
+
+    nam_vec <- vars_nam[[i]]
+
+    adam_db <-
+      adam_db %>%
+      dm(!!new_tab_nam := adsub_df, .name_repair = "unique")
+
+    adam_db <-
+      adam_db %>%
+      dm_zoom_to("adsl") %>%
+      left_join(!!new_tab_nam, by = keys, select = any_of(nam_vec)) %>%
+      dm_update_zoomed() %>%
+      dm_select_tbl(-!!new_tab_nam)
+  }
+
+  adam_db
+}
---
+++ tests/testthat/test-join_adsub_adsl.R
@@ -0,0 +1,52 @@
+adsl <- tibble::tibble(
+  USUBJID = c("S1", "S2", "S3", "S4"),
+  STUDYID = "My_study",
+  AGE = c(60, 44, 23, 31)
+)
+
+adsub <- tibble::tibble(
+  USUBJID = c("S1", "S2", "S3", "S4", "S1", "S2", "S3", "S99"),
+  STUDYID = "My_study",
+  PARAM = c("weight", "weight", "weight", "weight", "height", "height", "height", "weight"),
+  PARAMCD = c("w", "w", "w", "w", "h", "h", "h", "w"),
+  AVAL = c(98, 75, 70, 71, 182, 155, 152, 50),
+  AVALC = c(">80", "<=80", "<=80", "<=80", ">180", "<=180", "<=180", "<=80")
+)
+
+db <- dm::dm(adsl, adsub)
+
+db <- dm_add_pk(db, adsl, STUDYID)
+db <- dm_add_fk(db, adsub, STUDYID, adsl)
+
+test_that("join_adsub_adsl works as expected with default values", {
+  res <- expect_silent(join_adsub_adsl(adam_db = db))
+  checkmate::expect_class(res, "dm")
+  checkmate::expect_subset(names(res$adsl), c("USUBJID", "STUDYID", "AGE", "w", "h", "w_CAT", "h_CAT"))
+})
+
+test_that("join_adsub_adsl works as expected when no column is selected", {
+  res <- expect_silent(join_adsub_adsl(adam_db = db, continuous_var = NULL))
+  checkmate::expect_class(res, "dm")
+  checkmate::expect_subset(names(res$adsl), c("USUBJID", "STUDYID", "AGE", "w_CAT", "h_CAT"))
+
+  res <- expect_silent(join_adsub_adsl(adam_db = db, categorical_var = NULL))
+  checkmate::expect_class(res, "dm")
+  checkmate::expect_subset(names(res$adsl), c("USUBJID", "STUDYID", "AGE", "w", "h"))
+
+  res <- expect_silent(join_adsub_adsl(adam_db = db, continuous_var = NULL, categorical_var = NULL))
+  checkmate::expect_class(res, "dm")
+  checkmate::expect_subset(names(res$adsl), c("USUBJID", "STUDYID", "AGE"))
+})
+
+test_that("join_adsub_adsl throw a warning when column already exist in adsl.", {
+  new_db <- db %>%
+    dm::dm_zoom_to("adsl") %>%
+    mutate(h = 160) %>%
+    dm::dm_update_zoomed()
+
+  expect_warning(join_adsub_adsl(adam_db = new_db))
+})
+
+test_that("join_adsub_adsl throw a warning when two new columns would have the same name.", {
+  expect_warning(join_adsub_adsl(adam_db = db, continuous_suffix = "_x", categorical_suffix = "_x"))
+})
