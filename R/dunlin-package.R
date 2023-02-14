f64cab8989a181829eff55121f92d6fcfc615a53
Pawel Rucki <pawelrucki@gmail.com>
2023-02-14 12:01:58 +0100

    remove deprecated fun from NAMESPACE

--- R/dunlin-package.R
+++ R/dunlin-package.R
@@ -7,7 +7,7 @@
 #' @importFrom assertthat assert_that on_failure<-
 #' @importFrom tibble tribble
 #' @importFrom checkmate assert_class
-#' @importFrom forcats fct_explicit_na fct_relevel
+#' @importFrom forcats fct_relevel
 #' @importFrom magrittr %>%
 #' @importFrom methods is
 #' @importFrom stats setNames
