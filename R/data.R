175b228aab61799846f874568d63f6870fefd922
Liming <liming.li@roche.com>
2023-02-15 08:42:02 +0000

    remove assertion of mappings

--- R/data.R
+++ R/data.R
@@ -2,6 +2,6 @@
 
 #' Example of Remapping List
 #'
-#' @format a `list` of 2 elements with 3 hierarchical levels satisfying the criteria of the `assert_reformat`.
+#' @format a `rules` of 2 `rule`.
 #' @source this object is based on the `yaml` file of the same name stored in `inst/extdata`.
 "standard_remap"
--- data-raw/standard_remap.R
+++ data-raw/standard_remap.R
@@ -1,6 +1,6 @@
 
 # standard_remap ----
 
-standard_remap <- yaml::read_yaml(system.file("extdata", "standard_remap.yaml", package = "dunlin"))
+standard_remap <- read_rules(system.file("extdata", "standard_remap.yaml", package = "dunlin"))
 
 usethis::use_data(standard_remap, overwrite = TRUE)
