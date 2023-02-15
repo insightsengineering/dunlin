1d52c5b65c48c13b99ba5970d39832aaf407d141
Liming <liming.li@roche.com>
2023-02-15 14:40:28 +0000

    update style?no issues from my side actually

--- data-raw/standard_remap.R
+++ data-raw/standard_remap.R
@@ -1,6 +1,8 @@
 
 # standard_remap ----
 
-standard_remap <- read_rules(system.file("extdata", "standard_remap.yaml", package = "dunlin"))
+standard_remap <- read_rules(
+  system.file("extdata", "standard_remap.yaml", package = "dunlin")
+)
 
 usethis::use_data(standard_remap, overwrite = TRUE)
