d51b4505766319688e8a5fd0bf7a9400b51f4d51
Liming Li <liming.li@roche.com>
2023-04-18 06:02:05 +0000

    update filter functions

--- R/filter.R
+++ R/filter.R
@@ -1,7 +1,7 @@
 #' Filter Data with Log
 #' @param data (`data.frame`) input data to subset, or named (`list`) of (`data.frame`).
 #' @param table (`string`) table name.
-#' @param condition (`expression`) subset condition. Must eval as logical.
+#' @param condition (`call`) of subset condition. Must eval as logical.
 #'
 #' @details
 #' `log_filter` will filter the data/named list of data according to the `condition`.
@@ -15,29 +15,33 @@ log_filter <- function(data, condition, ...) {
 }
 
 #' @export
+#' @example
+#' log_filter(iris, Sepal.Length >= 7)
 log_filter.data.frame <- function(data, condition, ...) {
-  checkmate::assert_class(condition, "expression")
+  condition <- match.call()$condition
   vars <- all.vars(condition)
   var_in_env <- vapply(vars, exists, envir = parent.frame(), inherits = TRUE, FUN.VALUE = TRUE)
   var_in_data <- vapply(vars, `%in%`, table = names(data), FUN.VALUE = TRUE)
   if (!all(var_in_env | var_in_data)) {
-    stop("All vars in expression must exist in data!")
+    stop(sprintf("Variable %s not found in data or environment.", toString(vars[!(var_in_data | var_in_env)])))
   }
   res <- do.call(subset, list(x = data, subset = condition))
-  attr(res, "rows") <- c(attr(res, "rows"), list(c(nrow(data), nrow(res))))
+  attr(res, "rows") <- c(attr(data, "rows"), list(c(nrow(data), nrow(res))))
   res
 }
 
 #' @export
+#' @example
+#' log_filter(list(iris = iris), "iris", Sepal.Length >= 7)
 log_filter.list <- function(data, table, condition, ...) {
   checkmate::assert_list(data, types = "data.frame", names = "unique")
   checkmate::assert_subset(table, names(data))
-  checkmate::assert_class(condition, "expression")
-  data[[table]] <- log_filter(data[[table]], condition)
+  condition <- match.call()$condition
+  data[[table]] <- eval(bquote(log_filter(data[[table]], .(condition))))
   if (identical(table, "adsl")) {
     for (k in setdiff(names(data), "adsl")) {
       if ("USUBJID" %in% names(data[[k]]) && "USUBJID" %in% names(data[["adsl"]])) {
-        data[[k]] <- data[[k]][data[[k]]$USUBJID %in% data[["adsl"]]$USUBJID]
+        data[[k]] <- data[[k]][data[[k]]$USUBJID %in% data[["adsl"]]$USUBJID, ]
       }
     }
   }
---
+++ tests/testthat/test-filter.R
@@ -0,0 +1,43 @@
+# log_filter ----
+
+test_that("log_filter works for single data.frame", {
+  df1 <- expect_silent(log_filter(iris, Sepal.Length >= 7))
+  df2 <- subset(iris, Sepal.Length >= 7)
+  expect_identical(df1, df2, ignore_attr = TRUE)
+  df1_attr <- attr(df1, "rows")
+  expect_identical(df1_attr, list(c(150L, 13L)))
+  df3 <- expect_silent(log_filter(df1, Sepal.Width >= 3))
+  df4 <- subset(df2, Sepal.Width >= 3)
+  expect_identical(df3, df4, ignore_attr = TRUE)
+  df3_attr <- attr(df3, "rows")
+  expect_identical(df3_attr, list(c(150L, 13L), c(13L, 9L)))
+})
+
+test_that("log_filter fails if variable outside data/environment", {
+  expect_error(log_filter(iris, a >= 7), "Variable a not found")
+  threshold <- 7
+  expect_silent(log_filter(iris, Sepal.Width >= threshold))
+})
+
+test_that("log_filter works for list of data.frame", {
+  df_raw <- list(iris = iris)
+  df1 <- expect_silent(log_filter(df_raw, "iris", Sepal.Length >= 7))
+  df2 <- subset(iris, Sepal.Length >= 7)
+  expect_identical(df1$iris, df2, ignore_attr = TRUE)
+  df1_attr <- attr(df1$iris, "rows")
+  expect_identical(df1_attr, list(c(150L, 13L)))
+  df3 <- expect_silent(log_filter(df1, "iris", Sepal.Width >= 3))
+  df4 <- subset(df2, Sepal.Width >= 3)
+  expect_identical(df3$iris, df4, ignore_attr = TRUE)
+  df3_attr <- attr(df3$iris, "rows")
+  expect_identical(df3_attr, list(c(150L, 13L), c(13L, 9L)))
+})
+
+test_that("log_filter subset USUBJID for list of data.frame", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  df1 <- expect_silent(log_filter(df_raw, "adsl", b >= 7))
+  expect_identical(df1$adsl$USUBJID, c("g", "h", "i", "j"))
+  expect_identical(df1$dfb$USUBJID, c("g", "h", "i", "j"))
+})
