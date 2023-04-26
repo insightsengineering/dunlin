705f23138e33f378442a17cd5e3fff55647614a3
benoit <benoit.falquet@roche.com>
2023-04-26 12:13:51 +0200

    add test and pkgdown doc

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -46,4 +46,117 @@ test_that("log_filter subset USUBJID for list of data.frame", {
   df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
   expect_identical(df1$adsl$USUBJID, c("g", "h", "i", "j"))
   expect_identical(df1$dfb$USUBJID, c("g", "h", "i", "j"))
+
+  expect_identical(attr(df1$adsl, "rows"), list("b >= 7" = c(10L, 4L)))
+  expect_identical(attr(df1$dfb, "rows"), list("Filtered by adsl: b >= 7" = c(10L, 4L)))
+})
+
+# get_log ----
+
+test_that("get_log works as expected", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
+  res <- expect_silent(get_log(df1))
+
+  expect_identical(res$adsl, "b >= 7 [10 --> 4 rows.]")
+  expect_identical(res$dfb, "Filtered by adsl: b >= 7 [10 --> 4 rows.]")
+
+  df2 <- expect_silent(log_filter(df1, c >= 3, "dfb", by = "USUBJID"))
+  res2 <- expect_silent(get_log(df2))
+
+  expect_identical(res2$adsl, "b >= 7 [10 --> 4 rows.]")
+  expect_identical(res2$dfb, c("Filtered by adsl: b >= 7 [10 --> 4 rows.]", "c >= 3 [4 --> 2 rows.]"))
+})
+
+# print_log ----
+
+test_that("print_log works as expected", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+
+  df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
+  res <- capture.output(print_log(df1))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  b >= 7 [10 --> 4 rows.]",
+      "  - dfb:",
+      "  Filtered by adsl: b >= 7 [10 --> 4 rows.]"
+    )
+  )
+
+  df2 <- expect_silent(log_filter(df1, c >= 3, "dfb", by = "USUBJID"))
+  res2 <- capture.output(print_log(df2))
+
+  expect_identical(
+    res2,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  b >= 7 [10 --> 4 rows.]",
+      "  - dfb:",
+      "  Filtered by adsl: b >= 7 [10 --> 4 rows.] ",
+      "  c >= 3 [4 --> 2 rows.]"
+    )
+  )
+})
+
+
+test_that("print_log works as expect when no filtering is performed", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  res_ori <- capture.output(print_log(df_raw))
+
+  expect_identical(
+    res_ori,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  No filtering [10 rows.]",
+      "  - dfb:",
+      "  No filtering [10 rows.]"
+    )
+  )
+
+  res_ori <- capture.output(print_log(df_raw, incl = FALSE))
+
+  expect_identical(
+    res_ori,
+    c(
+      "Filter Log:",
+      "  No filtering"
+    )
+  )
+
+  df1 <- expect_silent(log_filter(df_raw, c >= 7, "dfb", by = "USUBJID"))
+  res <- capture.output(print_log(df1))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  No filtering [10 rows.]",
+      "  - dfb:",
+      "  c >= 7 [10 --> 4 rows.]"
+    )
+  )
+
+  res <- capture.output(print_log(df1, incl = FALSE))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - dfb:",
+      "  c >= 7 [10 --> 4 rows.]"
+    )
+  )
 })
