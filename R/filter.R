0a76796dd06957f2674cb2851562255d12fb3344
b_falquet <64274616+BFalquet@users.noreply.github.com>
2023-05-04 14:01:19 +0200

    79 obtain log through getter (#84)
    close #79 
    
    - create a `get_log` and a `print_log` method.
    - add log information to tables that are filtered due to filtering in
    `adsl`.
    - fix the case where `by = character(0)` in filter log to avoid merging
    data from adsl into a secondary table.
    
    thank you for the review
    
    ---------
    
    Co-authored-by: benoit <benoit.falquet@roche.com>

--- R/filter.R
+++ R/filter.R
@@ -1,6 +1,7 @@
 #' Filter Data with Log
 #' @param data (`data.frame`) input data to subset, or named (`list`) of (`data.frame`).
 #' @param condition (`call`) of subset condition. Must eval as logical.
+#' @param suffix (`string`) optional argument describing the filter.
 #' @param ... further arguments to be passed to or from other methods.
 #'
 #' @details
@@ -18,7 +19,9 @@ log_filter <- function(data, condition, ...) {
 #' @export
 #' @examples
 #' log_filter(iris, Sepal.Length >= 7)
-log_filter.data.frame <- function(data, condition, ...) {
+log_filter.data.frame <- function(data, condition, suffix = NULL, ...) {
+  checkmate::assert_string(suffix, null.ok = TRUE)
+
   condition <- match.call()$condition
   vars <- all.vars(condition)
   var_in_env <- vapply(vars, exists, envir = parent.frame(), inherits = TRUE, FUN.VALUE = TRUE)
@@ -27,7 +30,7 @@ log_filter.data.frame <- function(data, condition, ...) {
     stop(sprintf("Variable %s not found in data or environment.", toString(vars[!(var_in_data | var_in_env)])))
   }
   res <- do.call(subset, list(x = data, subset = condition), envir = parent.frame())
-  rows <- list(c(nrow(data), nrow(res)))
+  rows <- list(list(init = nrow(data), final = nrow(res), suffix = suffix))
   names(rows) <- deparse(condition)
   attr(res, "rows") <- c(attr(data, "rows"), rows)
   res
@@ -39,18 +42,162 @@ log_filter.data.frame <- function(data, condition, ...) {
 #' @export
 #' @examples
 #' log_filter(list(iris = iris), Sepal.Length >= 7, "iris", character(0))
-log_filter.list <- function(data, condition, table, by = c("USUBJID", "STUDYID"), ...) {
+log_filter.list <- function(data, condition, table, by = c("USUBJID", "STUDYID"), suffix = NULL, ...) {
   checkmate::assert_list(data, types = "data.frame", names = "unique")
   checkmate::assert_subset(table, names(data))
   checkmate::assert_names(colnames(data[[table]]), must.include = by)
   condition <- match.call()$condition
-  data[[table]] <- eval(bquote(log_filter(data[[table]], .(condition))))
+  data[[table]] <- eval(bquote(log_filter(data[[table]], .(condition), .(suffix))))
   if (identical(table, "adsl")) {
     for (k in setdiff(names(data), "adsl")) {
       if (all(by %in% names(data[[k]]))) {
+        if (length(by) == 0) by <- intersect(names(data[[k]]), names(data$adsl))
+
+        ori_n <- nrow(data[[k]])
+        ori_att <- attr(data[[k]], "rows")
+
         data[[k]] <- merge(data[[k]], data$adsl[by], by = by, all.x = FALSE, all.y = FALSE, sort = FALSE)
+
+        rows <- list(list(init = ori_n, final = nrow(data[[k]]), suffix = suffix))
+        names(rows) <- paste0("Filtered by adsl: ", deparse(condition), collapse = "")
+        attr(data[[k]], "rows") <- c(ori_att, rows)
       }
     }
   }
   return(data)
+}
+
+# Get Log ----
+
+#' Get Log
+#'
+#' @param data (`list` of `data.frame` or `data.frame`) filtered with `log_filter`.
+#' @param incl (`flag`) should information about unfiltered `data.frame` be printed.
+#' @param incl.adsl (`flag`) should indication  of filtering performed through `adsl` be printed.
+#'
+#' @export
+get_log <- function(data, incl, incl.adsl) {
+  UseMethod("get_log")
+}
+
+#' @rdname get_log
+#' @export
+#' @examples
+#' data <- log_filter(iris, Sepal.Length >= 7, "xx")
+#' data <- log_filter(data, Sepal.Length < 2)
+#' data <- log_filter(data, Sepal.Length >= 2, "yy")
+#' get_log(data)
+#'
+get_log.data.frame <- function(data, incl = TRUE, incl.adsl = TRUE) {
+  checkmate::assert_flag(incl)
+
+  att <- attr(data, "rows")
+  if (!incl.adsl) {
+    sel <- grepl("Filtered by adsl", names(att))
+    att <- att[!sel]
+  }
+
+  if (length(att) != 0L) {
+    start_row <- lapply(att, "[[", "init")
+    end_row <- lapply(att, "[[", "final")
+    suffix <- lapply(att, "[[", "suffix")
+    suffix <- vapply(suffix, function(x) ifelse(is.null(x), "", paste0(x, ": ")), character(1))
+    res <- paste0(suffix, names(att), " [", start_row, " --> ", end_row, " rows.]")
+  } else if (incl) {
+    paste0("No filtering [", nrow(data), " rows.]")
+  } else {
+    NULL
+  }
+}
+
+
+#' @rdname get_log
+#' @export
+#' @examples
+#' data <- log_filter(
+#'   list(iris1 = iris, iris2 = iris),
+#'   Sepal.Length >= 7,
+#'   "iris1",
+#'   character(0),
+#'   "Sep"
+#' )
+#' get_log(data)
+#'
+get_log.list <- function(data, incl = TRUE, incl.adsl = TRUE) {
+  checkmate::assert_list(data, types = "data.frame", names = "unique")
+  checkmate::assert_flag(incl)
+
+  lapply(data, get_log, incl = incl, incl.adsl = incl.adsl)
+}
+
+# Print Log ----
+
+#' Print Log
+#'
+#' @inheritParams get_log
+#' @export
+#'
+print_log <- function(data, incl, incl.adsl) {
+  UseMethod("print_log")
+}
+
+#' @rdname print_log
+#' @export
+#' @examples
+#' data <- log_filter(iris, Sepal.Length >= 7, "Sep")
+#' print_log(data)
+print_log.data.frame <- function(data, incl = TRUE, incl.adsl = TRUE) {
+  checkmate::assert_flag(incl)
+
+  cat("Filter Log:")
+  cat(paste0("\n  ", get_log(data, incl = incl, incl.adsl = incl.adsl)))
+  cat("\n")
+  invisible()
+}
+
+#' @rdname print_log
+#' @export
+#' @examples
+#' data <- log_filter(
+#'   list(
+#'     adsl = iris,
+#'     iris2 = iris,
+#'     mtcars = mtcars,
+#'     iris3 = iris
+#'   ),
+#'   Sepal.Length >= 7,
+#'   "adsl",
+#'   character(0),
+#'   "adsl filter"
+#' )
+#' data <- log_filter(data, Sepal.Length >= 7, "iris2", character(0), "iris2 filter")
+#' print_log(data)
+#' print_log(data, incl = FALSE)
+#' print_log(data, incl.adsl = FALSE, incl = FALSE)
+print_log.list <- function(data, incl = TRUE, incl.adsl = TRUE) {
+  checkmate::assert_list(data, types = "data.frame", names = "unique")
+  checkmate::assert_flag(incl)
+
+  filter_log <- get_log(data, incl = incl, incl.adsl = incl.adsl)
+
+  if (!incl) {
+    filter_log <- filter_log[!vapply(filter_log, is.null, logical(1))]
+  }
+
+  cat("Filter Log:")
+  if (length(filter_log) == 0) {
+    cat("\n  No filtering")
+  } else {
+    mapply(
+      function(x, y) {
+        cat(paste0("\n  - ", x, ":"))
+        cat(paste0("\n  ", y, ""))
+      },
+      as.list(names(filter_log)),
+      filter_log
+    )
+  }
+  cat("\n")
+
+  invisible()
 }
--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -5,7 +5,8 @@ test_that("log_filter works for single data.frame", {
   df2 <- subset(iris, Sepal.Length >= 7)
   expect_identical(df1, df2, ignore_attr = TRUE)
   df1_attr <- attr(df1, "rows")
-  expect_identical(df1_attr, list("Sepal.Length >= 7" = c(150L, 13L)))
+  expect_identical(df1_attr, list("Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL)))
+
   df3 <- expect_silent(log_filter(df1, Sepal.Width >= 3))
   df4 <- subset(df2, Sepal.Width >= 3)
   expect_identical(df3, df4, ignore_attr = TRUE)
@@ -13,8 +14,20 @@ test_that("log_filter works for single data.frame", {
   expect_identical(
     df3_attr,
     list(
-      "Sepal.Length >= 7" = c(150L, 13L),
-      "Sepal.Width >= 3" = c(13L, 9L)
+      "Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL),
+      "Sepal.Width >= 3" = list(init = 13L, final = 9L, suffix = NULL)
+    )
+  )
+
+  df3 <- expect_silent(log_filter(df1, Sepal.Width >= 3, "Sepal filter"))
+  df4 <- subset(df2, Sepal.Width >= 3)
+  expect_identical(df3, df4, ignore_attr = TRUE)
+  df3_attr <- attr(df3, "rows")
+  expect_identical(
+    df3_attr,
+    list(
+      "Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL),
+      "Sepal.Width >= 3" = list(init = 13L, final = 9L, suffix = "Sepal filter")
     )
   )
 })
@@ -31,12 +44,28 @@ test_that("log_filter works for list of data.frame", {
   df2 <- subset(iris, Sepal.Length >= 7)
   expect_identical(df1$iris, df2, ignore_attr = TRUE)
   df1_attr <- attr(df1$iris, "rows")
-  expect_identical(df1_attr, list("Sepal.Length >= 7" = c(150L, 13L)))
+  expect_identical(df1_attr, list("Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL)))
   df3 <- expect_silent(log_filter(df1, Sepal.Width >= 3, "iris", by = NULL))
   df4 <- subset(df2, Sepal.Width >= 3)
   expect_identical(df3$iris, df4, ignore_attr = TRUE)
   df3_attr <- attr(df3$iris, "rows")
-  expect_identical(df3_attr, list("Sepal.Length >= 7" = c(150L, 13L), "Sepal.Width >= 3" = c(13L, 9L)))
+  expect_identical(
+    df3_attr,
+    list(
+      "Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL),
+      "Sepal.Width >= 3" = list(init = 13L, final = 9L, suffix = NULL)
+    )
+  )
+  df5 <- expect_silent(log_filter(df3, Sepal.Width >= 2, "iris", by = NULL, suffix = "Sepal filter"))
+  df5_attr <- attr(df5$iris, "rows")
+  expect_identical(
+    df5_attr,
+    list(
+      "Sepal.Length >= 7" = list(init = 150L, final = 13L, suffix = NULL),
+      "Sepal.Width >= 3" = list(init = 13L, final = 9L, suffix = NULL),
+      "Sepal.Width >= 2" = list(init = 9L, final = 9L, suffix = "Sepal filter")
+    )
+  )
 })
 
 test_that("log_filter subset USUBJID for list of data.frame", {
@@ -46,4 +75,151 @@ test_that("log_filter subset USUBJID for list of data.frame", {
   df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
   expect_identical(df1$adsl$USUBJID, c("g", "h", "i", "j"))
   expect_identical(df1$dfb$USUBJID, c("g", "h", "i", "j"))
+
+  expect_identical(attr(df1$adsl, "rows"), list("b >= 7" = list(init = 10L, final = 4L, suffix = NULL)))
+  expect_identical(
+    attr(df1$dfb, "rows"),
+    list("Filtered by adsl: b >= 7" = list(init = 10L, final = 4L, suffix = NULL))
+  )
+})
+
+# get_log ----
+
+test_that("get_log works as expected", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
+  res <- expect_silent(get_log(df1))
+
+  expect_identical(res$adsl, "b >= 7 [10 --> 4 rows.]")
+  expect_identical(res$dfb, "Filtered by adsl: b >= 7 [10 --> 4 rows.]")
+
+  df2 <- expect_silent(log_filter(df1, c >= 3, "dfb", by = "USUBJID"))
+  res2 <- expect_silent(get_log(df2))
+
+  expect_identical(res2$adsl, "b >= 7 [10 --> 4 rows.]")
+  expect_identical(res2$dfb, c("Filtered by adsl: b >= 7 [10 --> 4 rows.]", "c >= 3 [4 --> 2 rows.]"))
+})
+
+test_that("get_log works as expected with suffix", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID", "b filter"))
+  res <- expect_silent(get_log(df1))
+
+  expect_identical(res$adsl, "b filter: b >= 7 [10 --> 4 rows.]")
+  expect_identical(res$dfb, "b filter: Filtered by adsl: b >= 7 [10 --> 4 rows.]")
+
+  df2 <- expect_silent(log_filter(df1, c >= 3, "dfb", by = "USUBJID"))
+  res2 <- expect_silent(get_log(df2))
+
+  expect_identical(res2$adsl, "b filter: b >= 7 [10 --> 4 rows.]")
+  expect_identical(res2$dfb, c("b filter: Filtered by adsl: b >= 7 [10 --> 4 rows.]", "c >= 3 [4 --> 2 rows.]"))
+})
+
+# print_log ----
+
+test_that("print_log works as expected", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+
+  df1 <- expect_silent(log_filter(df_raw, b >= 7, "adsl", by = "USUBJID"))
+  res <- capture.output(print_log(df1))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  b >= 7 [10 --> 4 rows.]",
+      "  - dfb:",
+      "  Filtered by adsl: b >= 7 [10 --> 4 rows.]"
+    )
+  )
+
+  df2 <- expect_silent(log_filter(df1, c >= 3, "dfb", by = "USUBJID", "C filter"))
+  res2 <- capture.output(print_log(df2))
+
+  expect_identical(
+    res2,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  b >= 7 [10 --> 4 rows.]",
+      "  - dfb:",
+      "  Filtered by adsl: b >= 7 [10 --> 4 rows.] ",
+      "  C filter: c >= 3 [4 --> 2 rows.]"
+    )
+  )
+})
+
+
+test_that("print_log works as expect when no filtering is performed", {
+  dfa <- data.frame(USUBJID = letters[1:10], b = 1:10)
+  dfb <- data.frame(USUBJID = letters[1:10], c = 10:1)
+  df_raw <- list(adsl = dfa, dfb = dfb)
+  res_ori <- capture.output(print_log(df_raw))
+
+  expect_identical(
+    res_ori,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  No filtering [10 rows.]",
+      "  - dfb:",
+      "  No filtering [10 rows.]"
+    )
+  )
+
+  res_ori <- capture.output(print_log(df_raw, incl = FALSE))
+
+  expect_identical(
+    res_ori,
+    c(
+      "Filter Log:",
+      "  No filtering"
+    )
+  )
+
+  df1 <- expect_silent(log_filter(df_raw, c >= 7, "dfb", by = "USUBJID"))
+  res <- capture.output(print_log(df1))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  No filtering [10 rows.]",
+      "  - dfb:",
+      "  c >= 7 [10 --> 4 rows.]"
+    )
+  )
+
+  res <- capture.output(print_log(df1, incl = FALSE))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - dfb:",
+      "  c >= 7 [10 --> 4 rows.]"
+    )
+  )
+
+  df2 <- expect_silent(log_filter(df_raw, c >= 7, "dfb", by = "USUBJID", "C filter"))
+  res <- capture.output(print_log(df2))
+
+  expect_identical(
+    res,
+    c(
+      "Filter Log:",
+      "  - adsl:",
+      "  No filtering [10 rows.]",
+      "  - dfb:",
+      "  C filter: c >= 7 [10 --> 4 rows.]"
+    )
+  )
 })
