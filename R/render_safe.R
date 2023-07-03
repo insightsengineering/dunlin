7f558684228e821327ba26cbef789ca5c39a641b
Liming Li <liming.li@roche.com>
2023-07-03 01:20:09 +0000

    use render_safe function

---
+++ R/render_safe.R
@@ -0,0 +1,24 @@
+#' Safe transformer
+#' @keywords internal
+safe_transformer <- function(text, envir) {
+  if (!exists(text, envir = envir)) {
+    text
+  }
+  else {
+    get(text, envir = envir)
+  }
+}
+
+#' Render whiskers safely
+#' @param x (`character`) input to be rendered safely.
+#' @param envir (`environment`) in which input is rendered.
+#' @export
+render_safe <- function(x, envir = parent.frame(), ...) {
+  vapply(
+    x,
+    glue::glue,
+    .transformer = safe_transformer,
+    .envir = envir,
+    FUN.VALUE = ""
+  )
+}
