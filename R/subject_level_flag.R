79b88954f74d3f9e55b1c8ce3b92e9673791c344
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-04-30 12:25:37 -0700

    Adding more details to docs

--- R/subject_level_flag.R
+++ R/subject_level_flag.R
@@ -1,14 +1,21 @@
 #' Create Subject-level Flag from Long Data
 #'
+#' @description
 #' Utility for creating subject-level flags from data frames that are
 #' more than one line per subject.
 #' For example, use this function to create a flag indicating whether a
 #' subject experienced any serious adverse events.
+#' 
+#' The function works by first creating a logical variable in `data_long`
+#' indicating whether the condition passed in the `...` argument is met.
+#' If a subject has true on any row, then the new variable is added
+#' to `data` as `TRUE`, otherwise that subject's value is populated with a `FALSE`.
 #'
 #' @param data (`data.frame`)\cr
 #'   a subject-level data frame, e.g. `adsl`
 #' @param data_long (`data.frame`)\cr
-#'   a long data frame that is more than one line per subject, e.g. `adae`
+#'   a long data frame that is more than one line per subject, e.g. `adae`.
+#'   The expressions passed in `...` will be evaluated in this data frame.
 #' @param ... ([`data-masking`][rlang::args_data_masking])\cr
 #'   Name-flag pairs. The name gives the name of the new subject-level
 #'   flag column. The value is a condition that results in a logical vector.
